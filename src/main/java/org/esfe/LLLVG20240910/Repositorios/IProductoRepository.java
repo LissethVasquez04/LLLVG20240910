5f2535bc9ea525aa2f80a41f647e370b85ffabf7
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-10 15:55:01 -0600

    Carpeta Repositorios terminada

---
+++ src/main/java/org/esfe/LLLVG20240910/Repositorios/IProductoRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.esfe.AMMA202409018.modelos.*;
+
+
+public interface IProductoRepository extends JpaRepository<ProductLLVG, Integer>  {
+
+}
+
