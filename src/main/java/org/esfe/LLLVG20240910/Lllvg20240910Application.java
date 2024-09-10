91e8dea2cf960c66dd9a5dde1d2779b5247c2105
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-09 20:57:45 -0600

    first commit

---
+++ src/main/java/org/esfe/LLLVG20240910/Lllvg20240910Application.java
@@ -0,0 +1,13 @@
+package org.esfe.LLLVG20240910;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lllvg20240910Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lllvg20240910Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/LLLVG20240910/Lllvg20240910ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.LLLVG20240910;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lllvg20240910ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
