0b163457b2d27f07c548a26e3257297ae6132ea3
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-09 22:50:52 -0600

    Modelo listo

---
+++ src/main/java/org/esfe/LLLVG20240910/Modelos/ProductLLVG.java
@@ -0,0 +1,24 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "productos")
+public class ProductLLVG {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre_llvg", nullable = false)
+    private String nombreLLVG;
+
+    @Column(name = "descripcion_llvg")
+    private String descripcionLLVG;
+
+    @Column(name = "precio", nullable = false)
+    private BigDecimal precio;
+}
