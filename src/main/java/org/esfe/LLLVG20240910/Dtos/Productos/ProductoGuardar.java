68e762889151b09fa3bf25623f3311984cc193ae
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-10 09:47:16 -0600

    DTOS Terminado

---
+++ src/main/java/org/esfe/LLLVG20240910/Dtos/Productos/ProductoGuardar.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+
+public class ProductoGuardar implemenst Serializable{
+private string nombreLLVG;
+private string descripcionLLVG;
+private BigDecimal precio;
+}
---
+++ src/main/java/org/esfe/LLLVG20240910/Dtos/Productos/ProductoSalida.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductoSalida implemenst Serializable {
+private Integer id;
+private string nombreLLVG;
+private string descripcionLLVG;
+private BigDecimal precio;
+}
